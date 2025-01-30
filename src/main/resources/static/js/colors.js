92d08cee87a609a9622df45f4eb4a27380a73b41
Minseok Kim <alstjr706@naver.com>
2025-01-31 08:59:27 +0900

    chore: (주석 제거) 함수마다 필요 주석만 추가

--- src/main/resources/static/js/colors.js
+++ src/main/resources/static/js/colors.js
@@ -1,16 +1,9 @@
-// 포스트잇 스타일 배경색 배열 (랜덤 색상 적용을 위해 사용)
 export const postItColors = ['#FFF6B8', '#FFD3B8', '#A9D4FF', '#FFC2C2', '#F4E2FF'];
 
-/**
- * 카드 리스트에 랜덤 배경색을 적용하는 함수
- * @param {NodeList} cards - .job-card 요소 리스트
- */
+// 카드 리스트에 랜덤 배경색 적용
 export function applyRandomColors(cards) {
     cards.forEach(card => {
-        // postItColors 배열에서 랜덤한 색상을 선택
         const randomColor = postItColors[Math.floor(Math.random() * postItColors.length)];
-
-        // 선택한 색상을 카드의 배경색으로 설정
         card.style.backgroundColor = randomColor;
     });
 }
