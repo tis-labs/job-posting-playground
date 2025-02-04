f4d6f3d1ae22204fc03b817ae0df8ef41cadf222
Minseok Kim <alstjr706@naver.com>
2025-02-04 17:06:03 +0900

    refactor: 불필요한 데이터 제거 (title, company, location, description)

--- src/main/java/dev/jobposting/playground/domain/JobPosting.java
+++ src/main/java/dev/jobposting/playground/domain/JobPosting.java
@@ -7,9 +7,5 @@ import lombok.Getter;
 @Builder
 public class JobPosting {
     private final Long id;
-    private final String title;
-    private final String company;
-    private final String location;
-    private final String description;
     private final int clickCount;
 }
