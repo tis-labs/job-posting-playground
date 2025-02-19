3504ec1f8106e2c7d73be794eb00a6dd1c0d9711
Minseok Kim <alstjr706@naver.com>
2025-02-20 04:40:21 +0900

    refactor: LB 없이도 동작하도록 수정

--- src/main/java/dev/jobposting/playground/geo/GeoLocationFilter.java
+++ src/main/java/dev/jobposting/playground/geo/GeoLocationFilter.java
@@ -39,7 +39,9 @@ public class GeoLocationFilter implements Filter {
     private String getClientIp(HttpServletRequest request) {
         String ip = request.getHeader("X-Forwarded-For");
 
-        if (ip == null || ip.isEmpty() || "unknown".equalsIgnoreCase(ip)) {
+        if (ip != null && !ip.isEmpty() && !"unknown".equalsIgnoreCase(ip)) {
+            ip = ip.split(",")[0].trim();
+        } else {
             ip = request.getRemoteAddr();
         }
 
