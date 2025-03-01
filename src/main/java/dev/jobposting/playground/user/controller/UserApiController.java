634b2d754181c0ee8e55257fb22e7d0c0623256b
kmularise <kmula222@gmail.com>
2025-03-01 15:34:48 +0900

    feat: 사용자 클래스 추가 - snowflake 설정 추가 - 사용자 open 이벤트 컨트롤러 추가

---
+++ src/main/java/dev/jobposting/playground/user/controller/UserApiController.java
@@ -0,0 +1,34 @@
+package dev.jobposting.playground.user.controller;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import dev.jobposting.playground.domain.user.UserIdMaker;
+import dev.jobposting.playground.user.config.UserLocation;
+import dev.jobposting.playground.user.dto.UserIdResponseDto;
+import dev.jobposting.playground.user.dto.UserLocationDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
+@Slf4j
+public class UserApiController {
+	private final UserIdMaker userIdMaker;
+
+	@PostMapping("users/id")
+	public UserIdResponseDto issueUserId() {
+		String userId = userIdMaker.generate();
+		return new UserIdResponseDto(userId);
+	}
+
+	@PostMapping("events/users/{id}/postings/open")
+	public UserIdResponseDto open(@PathVariable String id
+		,  @UserLocation UserLocationDto userLocationDto
+	) {
+		return new UserIdResponseDto(id);
+	}
+}
